2714c18a928bba3d77688f9abfdb4b79af32fde3
Ant2965 <antrikshbahri@gmail.com>
2023-10-03 13:36:29 +0530

    iven an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
    Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
    
    Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
    Return k.

---
+++ duplicate.java
@@ -0,0 +1,28 @@
+public class duplicate {
+
+    public static int removeDuplicates(int[] nums) {
+        int count = 1;
+        int a = 1;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] != nums[i + 1]) {
+
+                nums[a] = nums[i + 1];
+
+                a = a + 1;
+                count += 1;
+
+            }
+
+        }
+        return count;
+    }
+
+    public static void main(String args[]) {
+        int nums[] = { 1, 1, 2, 3, 4, 4, 5, 6, 6, 7, 7, 7 };
+        System.out.print(removeDuplicates(nums));
+
+    }
+
+}
