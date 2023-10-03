32d2f3a8101ec979d601827f4becc7697a7d5b92
Ant2965 <antrikshbahri@gmail.com>
2023-10-03 14:08:53 +0530

    power by recursion

--- power.java
+++ power.java
@@ -2,17 +2,24 @@ import java.util.*;
 
 public class power {
 
-    public static double powe(int x, int n) {
+    public static int powe(int x, int n, int i, int p) {
 
-        return Math.pow(x, n);
+        if (i == n) {
+            return p * x;
+        }
+        p = p * x;
+
+        return powe(x, n, i + 1, p);
 
     }
 
     public static void main(String args[]) {
 
         int x = 2;
-        int n = 3;
-        System.out.println(powe(x, n));
+        int n = 5;
+        int i = 1;
+        int p = 1;
+        System.out.println(powe(x, n, i, p));
 
     }
 
