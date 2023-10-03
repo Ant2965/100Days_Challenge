fcf88348c2132d3c2f3637af5396bd1c5da435a2
Ant2965 <antrikshbahri@gmail.com>
2023-10-03 13:48:34 +0530

    power of x to n

---
+++ power.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class power {
+
+    public static double powe(int x, int n) {
+
+        return Math.pow(x, n);
+
+    }
+
+    public static void main(String args[]) {
+
+        int x = 2;
+        int n = 3;
+        System.out.println(powe(x, n));
+
+    }
+
+}
