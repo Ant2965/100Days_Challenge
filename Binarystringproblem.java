02073391eb6adbf972f2209743ee6355a488c756
Ant2965 <antrikshbahri@gmail.com>
2023-10-05 18:35:01 +0530

    This the recursive approach for Binarystringproblem

---
+++ Binarystringproblem.java
@@ -0,0 +1,30 @@
+//package recursion;
+public class Binarystringproblem {
+
+    public static void bin(int n, int lastplace, String newstr) {
+        if (n == 0) {
+            System.out.println(newstr);
+            return;
+
+        }
+
+        if (lastplace == 0) {
+            bin(n - 1, 0, newstr + "0");
+
+            bin(n - 1, 1, newstr + "1");
+
+        } else {
+            bin(n - 1, 0, newstr + "0");
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        String newstr = "";
+
+        bin(n, 0, newstr);
+    }
+
+}
