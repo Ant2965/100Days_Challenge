299fefb27316eb8587aca9b18c6245c8de259196
Ant2965 <antrikshbahri@gmail.com>
2023-10-18 02:16:09 +0530

    Here i solved Findind index in rotated array

---
+++ Rotatedarray.java
@@ -0,0 +1,46 @@
+//package arraycreate;
+
+public class Rotatedarray {
+    public static int search(int arr[], int tar, int si, int ei) {
+        if (si > ei) {
+            return -1;
+        }
+
+        int mid = (si + ei) / 2;
+
+        if (arr[mid] == tar) {
+            return mid;
+
+        }
+
+        if (arr[si] <= arr[mid]) {
+            if (arr[si] <= tar && arr[mid] >= tar) {
+                return search(arr, tar, si, mid - 1);
+            } else {
+                return search(arr, tar, mid + 1, ei);
+
+            }
+
+        } else {
+            if (tar >= arr[mid] && tar <= arr[ei]) {
+
+                return search(arr, tar, mid + 1, ei);
+
+            } else {
+                return search(arr, tar, si, mid - 1);
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2, 3 };
+        int target = 0;
+
+        System.out.println(search(arr, target, 0, arr.length - 1));
+
+    }
+
+}
