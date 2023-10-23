4ec3d9782d961232115dd0d0d20f6803d6fb5e6b
Ant2965 <antrikshbahri@gmail.com>
2023-10-23 12:56:04 +0530

    Here i solved Minimum Absolute difference

---
+++ Absoluttedifference.java
@@ -0,0 +1,25 @@
+//package greedy;
+
+import java.util.Arrays;
+
+public class Absoluttedifference {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        int a[] = { 4, 1, 7, 8 };
+        int b[] = { 2, 3, 6, 5 };
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int difference = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            difference = difference + Math.abs(a[i] - b[i]);
+        }
+
+        System.out.println(difference);
+    }
+
+}
