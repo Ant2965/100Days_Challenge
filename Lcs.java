21020e9b5a654b94c27e92b4f3f02d335ba7f005
Ant2965 <antrikshbahri@gmail.com>
2023-10-28 23:42:38 +0530

    Started solving the LCS

---
+++ Lcs.java
@@ -0,0 +1,20 @@
+//package dp;
+
+public class Lcs {
+
+    public static int longestcommosubstring(String a, String b) {
+        int n = a.length();
+        int m = b.length();
+        int dp[][] = new int[n + 1][m + 1];
+
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        String a = "ABCDE";
+        String b = "ABGCE";
+
+    }
+
+}
