99ee75d2df5357b34d28d37c93ede4183e8f2711
Ant2965 <antrikshbahri@gmail.com>
2024-03-15 22:56:16 +0530

    Leftside view of tree

--- Leftside.java
+++ Leftside.java
@@ -1,3 +1,44 @@
+/*Left View of Binary Tree
+EasyAccuracy: 33.74%Submissions: 457K+Points: 2
+Share your experience with the world! Submit your admission, interview, campus or any other experience and reach an audience of millions today!
+
+banner
+Given a Binary Tree, return Left view of it. Left view of a Binary Tree is set of nodes visible when tree is visited from Left side. The task is to complete the function leftView(), which accepts root of the tree as argument. If no left view is possible, return an empty tree.
+
+Left view of following tree is 1 2 4 8.
+
+          1
+       /     \
+     2        3
+   /     \    /    \
+  4     5   6    7
+   \
+     8   
+
+Example 1:
+
+Input:
+   1
+ /  \
+3    2
+Output: 1 3
+
+Example 2:
+
+Input:
+
+Output: 10 20 40
+Your Task:
+You just have to complete the function leftView() that returns an array containing the nodes that are in the left view. The newline is automatically appended by the driver code.
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+0 <= Number of nodes <= 100
+0 <= Data of a node <= 1000 */
+
+
+
 //{ Driver Code Starts
     import java.util.LinkedList; 
     import java.util.Queue; 
