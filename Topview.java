69a793e50b9154087fa57fb93cb561097c7b6e7b
Ant2965 <antrikshbahri@gmail.com>
2024-05-12 22:49:42 +0530

    Done

--- Topview.java
+++ Topview.java
@@ -3,71 +3,63 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Topview {
-    public class node{
+    public class node {
         node left;
         node right;
         int data;
 
-
-
-
-        node(int data,node left,node right){
-            this.data=data;
-            this.right=null;
-            this.left=null;
-
+        node(int data, node left, node right) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
 
         }
 
     }
-    static class info{
+
+    static class info {
         node node;
         int hd;
 
-        info(node node,int hd){
-            this.node=node;
-            this.hd=hd;
+        info(node node, int hd) {
+            this.node = node;
+            this.hd = hd;
         }
     }
 
-    public static void topview(node root){
-        Queue<info>q=new LinkedList<>();
+    public static void topview(node root) {
+        Queue<info> q = new LinkedList<>();
 
-        HashMap<Integer , node>mao =new HashMap<>();
+        HashMap<Integer, node> mao = new HashMap<>();
 
-        int min=0,max=0;
-        q.add(new info(root,0));
+        int min = 0, max = 0;
+        q.add(new info(root, 0));
         q.add(null);
 
+        while (!q.isEmpty()) {
+            Inf curr = q.remove();
 
-
-        while(!q.isEmpty()){
-            Inf curr =q.remove();
-
-            if(curr==null){
-                if(q.isEmpty()){
+            if (curr == null) {
+                if (q.isEmpty()) {
                     break;
-                }else{
+                } else {
                     q.add(null);
                 }
             }
         }
 
+    }
+
+    public static void main(string args[]) {
+
+        node root = new root(1);
+        root.left = new node(2);
+        root.right = new node(3);
+        root.left.left = new node(4);
+        root.left.right = new node(5);
+        root.right.left = new node(6);
+        root.right.right = new node(7);
 
     }
 
-    public static void main(string args[]){
-
-        node root =new root(1);
-        root.left =new node(2);
-        root.right=new node(3);
-        root.left.left =new node(4);
-        root.left.right=new node(5);
-        root.right.left=new node(6);
-        root.right.right=new node(7);
-
-
-
-    }
-    
 }
--- test.java
+++ test.java
@@ -1,19 +1,18 @@
 public class test {
 
+    public static void main(String args[]) {
 
-    public static void main(String args[]){
+        int a[] = { 1, 2, 3, 45, 6 };
 
-        int a[]= {1,2,3,45,6};
-
-        for(int i=0;i<a.length;i++){
-            for(int j=i+1;j<a.length;j++){
-                if(a[i]+a[j]==9){
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[i] + a[j] == 9) {
                     System.out.println("yes");
                 }
-                
-                //System.out.println(a[i]);
+
+                // System.out.println(a[i]);
             }
         }
     }
-    
+
 }
