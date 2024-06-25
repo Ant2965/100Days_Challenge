932a3ce15ca6df1b4b1be7bf66871f322b9ee318
Ant2965 <antrikshbahri@gmail.com>
2024-06-25 11:29:58 +0530

    Jav abasic

---
+++ .java
@@ -0,0 +1,9 @@
+ class A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+
+        System.out.println(" Hi  i was testing that a java file name can be a  .jjava file");
+    }
+    
+}
